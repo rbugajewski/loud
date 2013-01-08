a5740444dc325143ab91bbbe3761e6aef5726838
Jered <me@jeredb.com>
2013-01-08 11:45:19 -0500

    Updated

--- feed/index.php
+++ feed/index.php
@@ -3,12 +3,13 @@ require_once 'FeedWriter.php';
 require_once '../Spyc.php';
 require_once '../functions.php';
 
-$TestFeed = new FeedWriter(ATOM);
-$TestFeed->setTitle('Fuck-Huff-Duff. Very Private Sonar Feed.');
-$TestFeed->setLink('http://'.$_SERVER['HTTP_HOST'].'/'.get_subdir($_SERVER[PHP_SELF]).'/');
-#print_r(get_subdir($_SERVER[PHP_SELF]));
+$TestFeed = new FeedWriter(RSS2);
+$TestFeed->setTitle('Ping!');
+$TestFeed->setDescription('A Private Sonar Feed. A private HuffDuffer.');
+$TestFeed->setLink('http://'.$_SERVER['HTTP_HOST'].'/'.get_subdir($_SERVER[PHP_SELF],'').'/');
+#print_r(get_subdir($_SERVER[PHP_SELF],''));
 $TestFeed->setChannelElement('updated', date(DATE_ATOM , time()));
-$TestFeed->setChannelElement('author', array('name'=>'Juicy Cocktail (http://juicycocktail.com/)'));
+$TestFeed->setChannelElement('author', array('name'=>'Jeredb (http://jeredb.com/)'));
 
 $items = Spyc::YAMLLoad('../urls.yaml');
 foreach ($items as $item)
